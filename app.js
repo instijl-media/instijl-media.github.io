3fa1875b5411090ea1fa6d67bb9b9281aaddd287
InStijlMedia <34706357+InStijlMedia@users.noreply.github.com>
2022-08-19 09:11:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-import { ism_products_connector.init('[data-ism-connector-target]','85x85x2','233x233x2', getUrlParam('id'), getUrlParam('secret')) } from './product-connector/main.js';
+$(document).ready(function() {
+  // Variables
+  var shopId = getUrlParam('id');
+  var secret = getUrlParam('secret');
+  var dataJsonUrl = 'https://instijl-media.github.io/data.json';
+  var ismAccess = false;
+
+  // Functions
+  function getUrlParam(value){
+    var url_string = document.getElementById('ism_app').src; 
+    var url = new URL(url_string);
+    var c = url.searchParams.get(value);
+    return c
+  }
+  function include(filePath) {
+    const scriptTag = document.createElement("script");
+    scriptTag.src = filePath;
+    document.body.appendChild(scriptTag);
+	}
+
+  $.getJSON(dataJsonUrl, function(data) {
+    console.log(shopId+'=='+data['id']);
+    if(shopId == data['shopid']){
+    	console.log('ID confirmed');
+      if(secret == data['key']){
+      	var ismAccess = true;
+        $.each(data['plugins'], function(key, plugin) {
+          include(plugin['src']);
+        });
+      } else {
+        console.log('Your secret doesnt match with the key in our system. Please contact support@instijlmedia.nl');
+      }
+    } else {
+      console.log('Your ID doesnt match with the ID in our systeem. Please contact support@instijlmedia.nl');
+    }
+  });
+});
